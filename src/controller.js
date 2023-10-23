d21d7994098201e1fc6e6e5bac1317de35a67c39
lucassdifranco <difrancolucas7@gmail.com>
2023-10-23 20:47:21 -0300

    Implementacion manejo de excepciones

--- src/controller.js
+++ src/controller.js
@@ -7,30 +7,115 @@ class LibrosController{
         res.json(result);
     }
     // MOSTRAR SÓLO UN LIBRO
-    async getOne(req, res){
+   async getOne(req, res) {
+    try {
         const libro = req.body;
         const id_libro = parseInt(libro.id);
-        const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`, [id_libro]);
+        if (isNaN(id_libro)) {
+            throw new Error('id invalido');
+        }
+
+        const [result] = await pool.query(`SELECT * FROM libros WHERE id = ?`, [id_libro]);
+        if (result.length === 0) {
+            throw new Error('Libro no encontrado');
+        }
+
         res.json(result);
-            }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
     // AGREGAR UN LIBRO
-    async add(req, res){
+ async add(req, res) {
+    try {
         const libro = req.body;
-        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES(?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anopublicacion, libro.isbn]);
-        res.json({"Libro Instertado": result.insertId});
+        const missingAttributes = [];
+        const invalidAttributes = [];
+
+        // Defino los atributos esperados
+        const expectedAttributes = ['nombre', 'autor', 'categoria', 'anopublicacion', 'isbn'];
+
+        // chequea atributos invalidos o faltantes
+        for (const attr in libro) {
+            if (!expectedAttributes.includes(attr)) {
+                invalidAttributes.push(attr);
+            }
+        }
+
+        if (missingAttributes.length > 0) {
+            throw new Error(`Faltan Datos: ${missingAttributes.join(', ')}`);
+        }
+
+        if (invalidAttributes.length > 0) {
+            throw new Error(`Datos invalidos: ${invalidAttributes.join(', ')}`);
+        }
+
+        const [result] = await pool.query(
+            `INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES(?, ?, ?, ?, ?)`,
+            [libro.nombre, libro.autor, libro.categoria, libro.anopublicacion, libro.isbn]
+        );
+
+        res.json({ "Libro Insertado": result.insertId });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
+}
+              
     // BORRAR UN LIBRO
-   async delete(req, res){
-       const libro = req.body;
-       const [result] = await pool.query(`DELETE FROM libros WHERE id=(?)`, [libro.id]);
-       res.json({"lIBRO ELIMINADO": result.affectedRows});
-   }
-    // ACTUALIZAR UN LIBRO
-    async update(req, res){
+   async delete(req, res) {
+    try {
         const libro = req.body;
-        const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), anopublicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anopublicacion, libro.isbn, libro.id]);
-        res.json({"LIBRO ACTUALIZADO": result.changedRows});
+
+        if (!libro.isbn || typeof libro.isbn !== 'string') {
+            throw new Error('DATO MAL INGRESADO');
+        }
+
+        const [result] = await pool.query(`DELETE FROM libros WHERE isbn = ?`, [libro.isbn]);
+
+        if (result.affectedRows === 0) {
+            throw new Error('NO SE ENCONTRO ISBN');
+        }
+
+        res.json({ "LIBRO ELIMINADO": result.affectedRows });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
+}
+    // ACTUALIZAR UN LIBRO
+    async update(req, res) {
+    try {
+        const libro = req.body;
+
+        // Define los atributos que pueden ser actualizados
+        const expectedAttributes = ['nombre', 'autor', 'categoria', 'anopublicacion', 'isbn', 'id'];
+
+        // Chequea si hay atributos que no esten en la lista
+        for (const attr in libro) {
+            if (!expectedAttributes.includes(attr)) {
+                throw new Error(`Dato invalidos: ${attr}`);
+            }
+        }
+
+        // Chequea si falta el id del libro
+        if (!libro.id) {
+            throw new Error('AGREGUE EL "ID" DEL LIBRO A MODIFICAR');
+        }
+
+        const [result] = await pool.query(
+            `UPDATE libros SET nombre = ?, autor = ?, categoria = ?, anopublicacion = ?, isbn = ? WHERE id = ?`,
+            [libro.nombre, libro.autor, libro.categoria, libro.anopublicacion, libro.isbn, libro.id]
+        );
+
+        if (result.changedRows === 0) {
+            throw new Error('ID no encontrado');
+        }
+
+        res.json({ "LIBROS ACTUALIZADOS": result.changedRows });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
     
         }
     
--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ app.use(router);
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 })
+
