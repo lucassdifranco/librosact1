764c465346680dbf8b53306c3599ebbf33ca331e
lucassdifranco <difrancolucas7@gmail.com>
2023-10-23 15:44:53 -0300

    agregar libros

--- src/controller.js
+++ src/controller.js
@@ -11,6 +11,17 @@ class LibrosController{
         const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`, [id_libro]);
         res.json(result);
             }
+    async add(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES(?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anopublicacion, libro.isbn]);
+        res.json({"Libro Instertado": result.insertId});
+    }
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM libros Where id=(?)`, [libro.id]);
+        res.json({"Registros eliminados": result.affectedRows});
+    }
+    
         }
     
   
